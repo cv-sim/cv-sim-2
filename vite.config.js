aa9b0550ce2590b051ad44acff1a430133d7135e
Leo Smith <leosmith36@yahoo.com>
2024-07-06 17:59:56 -0500

    added vite plugin to fix html for deploy

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,12 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
+    {
+      name: 'html-transform',
+      transformIndexHtml(html) {
+        return html.replace(/\/assets/g, './assets')
+      }
+    }
   ],
   resolve: {
     alias: {
