6329ad75cc3dfa02769a0d8a08a16d8c8d551be6
Leo Smith <leosmith36@yahoo.com>
2024-07-07 07:27:35 -0500

    page header ui fix for small screens

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -17,13 +17,13 @@ const store = useGraphStore()
 <template>
   <div class="flex flex-wrap gap-4">
     <h1 class="font-bold">Cyclic Voltammetry Simulator</h1>
-    <div class="ml-auto flex gap-4">
+    <div class="ml-auto flex flex-wrap gap-4">
       <InputSelect v-model="store.convention" :options="conventionOptions" label="Convention:" />
-      <button @click="() => store.reset()">
+      <button @click="() => store.reset()" class="self-center">
         <FontAwesomeIcon icon="rotate-left" class="mr-1" />
         Reset
       </button>
-      <button @click="() => store.export()">
+      <button @click="() => store.export()" class="self-center">
         <FontAwesomeIcon icon="file-export" class="mr-1" />
         Export
       </button>
